a628c494c835f5ec49933de45862790e0356200a
Thanh Vu <thanhauco@gmail.com>
2022-10-05 11:30:44 -0700

    feat(web): add root layout with dark theme

---
+++ packages/web/app/layout.tsx
@@ -0,0 +1,27 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Washington State Voting Portal",
+  description: "Secure, transparent, and verifiable online voting for Washington State elections",
+  keywords: ["voting", "election", "Washington", "secure", "blockchain"],
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en" className="dark">
+      <body className={inter.className}>
+        <div className="min-h-screen bg-gradient-to-br from-slate-900 via-purple-900 to-slate-900">
+          {children}
+        </div>
+      </body>
+    </html>
+  );
+}
